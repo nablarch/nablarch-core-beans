531d3f1da34371610b40ec1b481a367e847b5d66
Takayuki Uchida <uchida.takayuki@tis.co.jp>
2024-02-22 16:53:42 +0900

    test: createMapAndCopyIncludes, createMapAndCopyExcludesのテストを追加

--- src/test/java/nablarch/core/beans/BeanUtilForRecordTest.java
+++ src/test/java/nablarch/core/beans/BeanUtilForRecordTest.java
@@ -5,6 +5,7 @@ import nablarch.test.support.log.app.OnMemoryLogWriter;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -1387,10 +1388,85 @@ public class BeanUtilForRecordTest {
         assertThat(((InnerRecord[])dest.get("innerRecordArray"))[1].id, is(10005));
         assertThat(((InnerRecord[])dest.get("innerRecordArray"))[1].name, is("森川瑛太"));
 
+    }
 
+    @SuppressWarnings("unchecked")
+    @Test
+    public void 指定したパラメータのみ使用してレコードからMapを生成できること() {
 
+        SourceRecord srcRecord = new SourceRecord(
+                10,
+                "test",
+                new Address("111-2222", "東京都江東区"),
+                new InnerRecord(10001, "中田昇"),
+                List.of("1", "2"),
+                List.of(new Address("111-2222", "東京都新宿区"), new Address("333-4444", "兵庫県神戸市")),
+                List.of(new InnerRecord(10002, "武藤菊夜"), new InnerRecord(10003, "猪野麻天")),
+                new String[]{"3", "4"},
+                new Address[]{new Address("555-6666", "大阪府大阪市"), new Address("777-8888", "福岡県福岡市")},
+                new InnerRecord[]{new InnerRecord(10004, "神田幹太"), new InnerRecord(10005, "森川瑛太")}
+        );
 
+        Map<String, Object> dest = BeanUtil.createMapAndCopyIncludes(srcRecord, "sample", "strList", "innerRecordArray", "onlyInSourceRecord");
+
+        assertThat(dest.get("sample"), is(10));
+        assertThat(dest.get("onlyInSourceRecord"), is("test"));
+        assertThat(dest.containsKey("address"), is(false));
+        assertThat(dest.containsKey("innerRecord"), is(false));
+        assertThat(((List<String>)dest.get("strList")).get(0), is("1"));
+        assertThat(((List<String>)dest.get("strList")).get(1), is("2"));
+        assertThat(dest.containsKey("addressList"), is(false));
+        assertThat(dest.containsKey("innerRecordList"), is(false));
+        assertThat(dest.containsKey("strArray"), is(false));
+        assertThat(dest.containsKey("addressArray"), is(false));
+        assertThat(((InnerRecord[])dest.get("innerRecordArray"))[0].id, is(10004));
+        assertThat(((InnerRecord[])dest.get("innerRecordArray"))[0].name, is("神田幹太"));
+        assertThat(((InnerRecord[])dest.get("innerRecordArray"))[1].id, is(10005));
+        assertThat(((InnerRecord[])dest.get("innerRecordArray"))[1].name, is("森川瑛太"));
 
     }
 
+    @SuppressWarnings("unchecked")
+    @Test
+    public void 指定したパラメータを除外してレコードからMapを生成できること() {
+
+        SourceRecord srcRecord = new SourceRecord(
+                10,
+                "test",
+                new Address("111-2222", "東京都江東区"),
+                new InnerRecord(10001, "中田昇"),
+                List.of("1", "2"),
+                List.of(new Address("111-2222", "東京都新宿区"), new Address("333-4444", "兵庫県神戸市")),
+                List.of(new InnerRecord(10002, "武藤菊夜"), new InnerRecord(10003, "猪野麻天")),
+                new String[]{"3", "4"},
+                new Address[]{new Address("555-6666", "大阪府大阪市"), new Address("777-8888", "福岡県福岡市")},
+                new InnerRecord[]{new InnerRecord(10004, "神田幹太"), new InnerRecord(10005, "森川瑛太")}
+        );
+
+        Map<String, Object> dest = BeanUtil.createMapAndCopyExcludes(srcRecord, "sample", "strList", "innerRecordArray", "onlyInSourceRecord");
+
+        assertThat(dest.containsKey("sample"), is(false));
+        assertThat(dest.containsKey("onlyInSourceRecord"), is(false));
+        assertThat(dest.get("address.postCode"), is("111-2222"));
+        assertThat(dest.get("address.addr"), is("東京都江東区"));
+        assertThat(dest.get("innerRecord.id"), is(10001));
+        assertThat(dest.get("innerRecord.name"), is("中田昇"));
+        assertThat(dest.containsKey("strList"), is(false));
+        assertThat(((List<Address>)dest.get("addressList")).get(0).postCode, is("111-2222"));
+        assertThat(((List<Address>)dest.get("addressList")).get(0).addr, is("東京都新宿区"));
+        assertThat(((List<Address>)dest.get("addressList")).get(1).postCode, is("333-4444"));
+        assertThat(((List<Address>)dest.get("addressList")).get(1).addr, is("兵庫県神戸市"));
+        assertThat(((List<InnerRecord>)dest.get("innerRecordList")).get(0).id, is(10002));
+        assertThat(((List<InnerRecord>)dest.get("innerRecordList")).get(0).name, is("武藤菊夜"));
+        assertThat(((List<InnerRecord>)dest.get("innerRecordList")).get(1).id, is(10003));
+        assertThat(((List<InnerRecord>)dest.get("innerRecordList")).get(1).name, is("猪野麻天"));
+        assertThat(((String[])dest.get("strArray"))[0], is("3"));
+        assertThat(((String[])dest.get("strArray"))[1], is("4"));
+        assertThat(((Address[])dest.get("addressArray"))[0].postCode, is("555-6666"));
+        assertThat(((Address[])dest.get("addressArray"))[0].addr, is("大阪府大阪市"));
+        assertThat(((Address[])dest.get("addressArray"))[1].postCode, is("777-8888"));
+        assertThat(((Address[])dest.get("addressArray"))[1].addr, is("福岡県福岡市"));
+        assertThat(dest.containsKey("innerRecordArray"), is(false));
+
+    }
 }
