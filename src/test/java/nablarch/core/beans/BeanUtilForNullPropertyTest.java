e8ac9b552c3501d8dfa89ffb3925c524f8d91ee1
Takayuki Uchida <uchida.takayuki@tis.co.jp>
2024-02-28 17:42:12 +0900

    test: 既存動作確認テストの修正・追加

--- src/test/java/nablarch/core/beans/BeanUtilForNullPropertyTest.java
+++ src/test/java/nablarch/core/beans/BeanUtilForNullPropertyTest.java
@@ -15,7 +15,7 @@ import static org.junit.Assert.assertThrows;
 @SuppressWarnings("NonAsciiCharacters")
 public class BeanUtilForNullPropertyTest {
 
-    private static class SrcBean {
+    public static class SrcBean {
         private String name;
         public String getName() {
             return name;
@@ -25,7 +25,37 @@ public class BeanUtilForNullPropertyTest {
         }
     }
 
-    private static class DestBean {
+    public static class DestBean {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    public static class SrcParentBean {
+        private ChildBean childBean;
+        public ChildBean getChildBean() {
+            return childBean;
+        }
+        public void setChildBean(ChildBean childBean) {
+            this.childBean = childBean;
+        }
+    }
+
+    public static class DestParentBean {
+        private ChildBean childBean;
+        public ChildBean getChildBean() {
+            return childBean;
+        }
+        public void setChildBean(ChildBean childBean) {
+            this.childBean = childBean;
+        }
+    }
+
+    public static class ChildBean {
         private String name;
         public String getName() {
             return name;
@@ -82,56 +112,107 @@ public class BeanUtilForNullPropertyTest {
     }
 
     @Test
-    public void 移送元をMapとするcreateAndCopyIncludes_プロパティの指定がnullの場合は実行時例外が送出されること() {
+    public void 移送元をBeanとするcopy_コピー元beanがnullの場合はNPEが送出されること() {
+        DestBean dest = new DestBean();
+        dest.setName("value");
+
+        assertThrows(NullPointerException.class, () ->
+                BeanUtil.copy(null, dest, CopyOptions.empty()));
+    }
+
+    @Test
+    public void 移送元をBeanとするcopy_コピー先beanがnullの場合はNPEが送出されること() {
+        SrcBean src = new SrcBean();
+        src.setName("value");
+
+        assertThrows(NullPointerException.class, () ->
+                BeanUtil.copy(src, null, CopyOptions.empty()));
+    }
+
+    @Test
+    public void 移送元をBeanとするcopy_コピー元beanの子Beanがnullの場合はコピーされないこと() {
+        SrcParentBean src = new SrcParentBean();
+        src.setChildBean(null);
+        DestParentBean dest = new DestParentBean();
+        dest.setChildBean(new ChildBean());
+
+        BeanUtil.copy(src, dest, CopyOptions.empty());
+        // CopyOptions.empty().isExcludesNull() == false なので、nullのプロパティもコピーされるはずだが、Nablarch6時点ではではコピーされない。
+        assertThat(dest.getChildBean(), instanceOf(ChildBean.class));
+    }
+
+    @Test
+    public void 移送元をMapとするcreateAndCopyIncludes_プロパティの指定がStringの配列にキャストされたnullの場合は実行時例外が送出されること() {
         Map<String, Object> srcMap = new HashMap<>(){{put("name", "value");}};
 
         assertThrows(NullPointerException.class, () ->
                 BeanUtil.createAndCopyIncludes(DestBean.class, srcMap, (String[]) null));
 
-        BeansException beansException = assertThrows(BeansException.class, () ->
-                BeanUtil.createAndCopyIncludes(DestBean.class, srcMap, (String) null));
-        assertThat(beansException.getCause(), instanceOf(NoSuchMethodException.class));
-
     }
 
     @Test
-    public void 移送元をMapとするcreateAndCopyExcludes_プロパティの指定がnullの場合は実行時例外が送出されること() {
+    public void 移送元をMapとするcreateAndCopyIncludes_プロパティの指定がStringにキャストされたnullの場合は成功すること() {
+        Map<String, Object> srcMap = new HashMap<>(){{put("name", "value");}};
+
+        DestBean dest = BeanUtil.createAndCopyIncludes(DestBean.class, srcMap, (String) null);
+        assertThat(dest.getName(), is(nullValue()));
+    }
+
+    @Test
+    public void 移送元をMapとするcreateAndCopyExcludes_プロパティの指定がStringの配列にキャストされたnullの場合は実行時例外が送出されること() {
         Map<String, Object> srcMap = new HashMap<>(){{put("name", "value");}};
 
         assertThrows(NullPointerException.class, () ->
                 BeanUtil.createAndCopyExcludes(DestBean.class, srcMap, (String[]) null));
 
-        BeansException beansException = assertThrows(BeansException.class, () ->
-                BeanUtil.createAndCopyExcludes(DestBean.class, srcMap, (String) null));
-        assertThat(beansException.getCause(), instanceOf(NoSuchMethodException.class));
+    }
+
+    @Test
+    public void 移送元をMapとするcreateAndCopyExcludes_プロパティの指定がStringにキャストされたnullの場合は成功すること() {
+        Map<String, Object> srcMap = new HashMap<>(){{put("name", "value");}};
+
+        DestBean dest = BeanUtil.createAndCopyExcludes(DestBean.class, srcMap, (String) null);
+        assertThat(dest.getName(), is("value"));
 
     }
 
     @Test
-    public void 移送元をBeanとするcreateAndCopyIncludes_プロパティの指定がnullの場合は実行時例外が送出されること() {
+    public void 移送元をBeanとするcreateAndCopyIncludes_プロパティの指定がStringの配列にキャストされたnullの場合は実行時例外が送出されること() {
         SrcBean src = new SrcBean();
         src.setName("value");
 
         assertThrows(NullPointerException.class, () ->
                 BeanUtil.createAndCopyIncludes(DestBean.class, src, (String[]) null));
 
-        BeansException beansException = assertThrows(BeansException.class, () ->
-                BeanUtil.createAndCopyIncludes(DestBean.class, src, (String) null));
-        assertThat(beansException.getCause(), instanceOf(NoSuchMethodException.class));
+    }
+
+    @Test
+    public void 移送元をBeanとするcreateAndCopyIncludes_プロパティの指定がStringにキャストされたnullの場合は成功すること() {
+        SrcBean src = new SrcBean();
+        src.setName("value");
+
+        DestBean dest = BeanUtil.createAndCopyIncludes(DestBean.class, src, (String) null);
+        assertThat(dest.getName(), is(nullValue()));
 
     }
 
     @Test
-    public void 移送元をBeanとするcreateAndCopyExcludes_プロパティの指定がnullの場合は実行時例外が送出されること() {
+    public void 移送元をBeanとするcreateAndCopyExcludes_プロパティの指定がStringの配列にキャストされたnullの場合は実行時例外が送出されること() {
         SrcBean src = new SrcBean();
         src.setName("value");
 
         assertThrows(NullPointerException.class, () ->
                 BeanUtil.createAndCopyExcludes(DestBean.class, src, (String[]) null));
 
-        BeansException beansException = assertThrows(BeansException.class, () ->
-                BeanUtil.createAndCopyExcludes(DestBean.class, src, (String) null));
-        assertThat(beansException.getCause(), instanceOf(NoSuchMethodException.class));
+    }
+
+    @Test
+    public void 移送元をBeanとするcreateAndCopyExcludes_プロパティの指定がStringにキャストされたnullの場合は成功すること() {
+        SrcBean src = new SrcBean();
+        src.setName("value");
+
+        DestBean dest = BeanUtil.createAndCopyExcludes(DestBean.class, src, (String) null);
+        assertThat(dest.getName(), is("value"));
 
     }
 }
